905a5d0e5c9ffc653f163eba08c0e24f2bcced72
HighSierra <highsierra@HighSierras-Mac.local>
2018-07-16 23:37:20 -0700

    add screens

---
+++ src/screens/ActiveScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+export default class ActiveScreen extends Component {
+    state = {  }
+    render() {
+        return (
+            <View>
+                <Text>Active Screen</Text>
+            </View>
+        );
+    }
+}
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+export default class SplashScreen extends Component {
+    state = {  }
+    render() {
+        return (
+            <View>
+                <Text>Splash Screen</Text>
+            </View>
+        );
+    }
+}
